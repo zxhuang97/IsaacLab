ee0fb951ae75bce6cd9a8dde328cf62344126588
Zixuan Huang <cloudsky.marcus@gmail.com>
2025-02-04 23:20:43 -0500

    osc;factory_parameters

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/screw/config/kuka/ik_rel_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/screw/config/kuka/ik_rel_env_cfg.py
@@ -22,11 +22,15 @@ from pxr import Usd, UsdGeom
 from regex import F
 
 import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.sim.simulation_cfg import PhysxCfg, SimulationCfg
+from omni.isaac.lab.sim.spawners.materials.physics_materials_cfg import RigidBodyMaterialCfg
 import omni.isaac.lab.utils.math as math_utils
 from omni.isaac.lab.assets import Articulation, RigidObject
+
+from omni.isaac.lab.controllers.operational_space_cfg import OperationalSpaceControllerCfg
 from omni.isaac.lab.controllers.differential_ik_cfg import DifferentialIKControllerCfg
 from omni.isaac.lab.envs import ManagerBasedEnv
-from omni.isaac.lab.envs.mdp.actions.actions_cfg import DifferentialInverseKinematicsActionCfg
+from omni.isaac.lab.envs.mdp.actions.actions_cfg import DifferentialInverseKinematicsActionCfg, OperationalSpaceControllerActionCfg
 from omni.isaac.lab.managers import CurriculumTermCfg as CurrTerm
 from omni.isaac.lab.managers import EventTermCfg as EventTerm
 from omni.isaac.lab.managers import ManagerTermBase
@@ -330,7 +334,8 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
 
     def get_default_env_params(self):
         super().get_default_env_params()
-        self.params.sim.dt = self.params.sim.get("dt", 1.0 / 120.0)
+        self.params.sim.dt = self.params.sim.get("dt", 1.0 / 60.0)
+        self.params.use_factory_params = self.params.get("use_factory_params", False)
         self.params.scene.robot = self.params.scene.get("robot", OmegaConf.create())
         # self.pre_grasp_path
         robot_params = self.params.scene.robot
@@ -354,6 +359,7 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
         nut_params.rigid_grasp = nut_params.get("rigid_grasp", True)
 
         action_params = self.params.actions
+        action_params.controller_type = action_params.get("controller_type", "ik")
         action_params.ik_lambda = action_params.get("ik_lambda", 0.001)
         action_params.keep_grasp_state = action_params.get("keep_grasp_state", True)
         action_params.uni_rotate = action_params.get("uni_rotate", True)
@@ -399,6 +405,8 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
         # robot 
         self.scene.robot = KUKA_VICTOR_LEFT_HIGH_PD_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         robot = self.scene.robot
+        nut = self.scene.nut
+        bolt = self.scene.bolt
         robot_params = self.params.scene.robot
         if robot_params.collision_approximation == "convexHull":
             robot.spawn.usd_path = "assets/victor/victor_left_arm_with_gripper_v2/victor_left_arm_with_gripper_v2.usd"
@@ -413,12 +421,62 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
         robot.spawn.rigid_props.sleep_threshold = robot_params.sleep_threshold
         robot.spawn.rigid_props.stabilization_threshold = robot_params.stabilization_threshold
 
-        # robot.spawn.physics_material = materials.RigidBodyMaterialCfg(
-        #     static_friction=robot_params.static_friction,
-        #     dynamic_friction=robot_params.dynamic_friction,
-        #     compliant_contact_stiffness=robot_params.compliant_contact_stiffness,
-        #     compliant_contact_damping=robot_params.compliant_contact_damping,
-        # )
+        if self.params.use_factory_params:
+            self.sim = SimulationCfg(
+                dt=1 / 120,
+                physx=PhysxCfg(
+                    solver_type=1,
+                    max_position_iteration_count=192,  # Important to avoid interpenetration.
+                    max_velocity_iteration_count=1,
+                    bounce_threshold_velocity=0.2,
+                    friction_offset_threshold=0.01,
+                    friction_correlation_distance=0.00625,
+                    gpu_max_rigid_contact_count=2**23,
+                    gpu_max_rigid_patch_count=2**23,
+                    gpu_max_num_partitions=1,  # Important for stable simulation.
+                ),
+                physics_material=RigidBodyMaterialCfg(
+                    static_friction=1.0,
+                    dynamic_friction=1.0,
+                ),
+            )
+            robot.spawn.articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+                enabled_self_collisions=False,
+                solver_position_iteration_count=192,
+                solver_velocity_iteration_count=1,
+            )
+            robot.spawn.collision_props = sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0)
+            
+            nut.spawn.rigid_props=sim_utils.RigidBodyPropertiesCfg(
+                    disable_gravity=True,
+                    max_depenetration_velocity=5.0,
+                    linear_damping=0.0,
+                    angular_damping=0.0,
+                    max_linear_velocity=1000.0,
+                    max_angular_velocity=3666.0,
+                    enable_gyroscopic_forces=True,
+                    solver_position_iteration_count=192,
+                    solver_velocity_iteration_count=1,
+                    max_contact_impulse=1e32,
+                )
+            nut.spawn.mass_props=sim_utils.MassPropertiesCfg(mass=0.05)
+            nut.spawn.collision_props = sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0)
+            
+            bolt.spawn.rigid_props=sim_utils.RigidBodyPropertiesCfg(
+                    disable_gravity=False,
+                    max_depenetration_velocity=5.0,
+                    linear_damping=0.0,
+                    angular_damping=0.0,
+                    max_linear_velocity=1000.0,
+                    max_angular_velocity=3666.0,
+                    enable_gyroscopic_forces=True,
+                    solver_position_iteration_count=192,
+                    solver_velocity_iteration_count=1,
+                    max_contact_impulse=1e32,
+                )
+            bolt.spawn.mass_props=sim_utils.MassPropertiesCfg(mass=0.05)
+            bolt.spawn.collision_props = sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0)
+            
         robot.actuators["victor_left_arm"].stiffness = robot_params.arm_stiffness
         robot.actuators["victor_left_arm"].damping = robot_params.arm_damping
         robot.actuators["victor_left_gripper"].velocity_limit = 1
@@ -428,13 +486,13 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
 
         # action
         action_params = self.params.actions
-        # arm_lows = [-0.01, -0.01, -0.01, -0.05, -0.05, -0.5]
-        # arm_highs = [0.01, 0.01, 0.01, 0.05, 0.05, 0.5]
-        # scale = [0.01, 0.01, 0.01, 0.05, 0.05, 0.5]
-        arm_lows = [-0.004, -0.004, -0.004, -0.02, -0.02, -0.5]
-        arm_highs = [0.004, 0.004, 0.004, 0.02, 0.02, 0.5]
-        scale = [0.004, 0.004, 0.004, 0.02, 0.02, 0.5]
+        # arm_lows = [-0.004, -0.004, -0.004, -0.02, -0.02, -0.5]
+        # arm_highs = [0.004, 0.004, 0.004, 0.02, 0.02, 0.5]
+        # scale = [0.004, 0.004, 0.004, 0.02, 0.02, 0.5]
 
+        arm_lows = [-0.002, -0.002, -0.002, -0.01, -0.01, -0.4]
+        arm_highs = [0.002, 0.002, 0.002, 0.01, 0.01, 0.4]
+        scale = [0.002, 0.002, 0.002, 0.01, 0.01, 0.4]
         if self.params.events.reset_target == "rigid_grasp_open_tilt" or \
                 self.params.events.reset_joint_std > 0:
             arm_lows = [-0.002, -0.002, -0.002, -0.01, -0.01, -0.5]
@@ -444,11 +502,12 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
         if action_params.uni_rotate:
             arm_highs[5] = 0.0
 
-        self.actions.arm_action = DifferentialInverseKinematicsActionCfg(
-            asset_name="robot",
-            joint_names=["victor_left_arm_joint.*"],
-            body_name="victor_left_tool0",
-            controller=DifferentialIKControllerCfg(
+        if action_params.controller_type == "ik":
+            self.actions.arm_action = DifferentialInverseKinematicsActionCfg(
+                asset_name="robot",
+                joint_names=["victor_left_arm_joint.*"],
+                body_name="victor_left_tool0",
+                controller=DifferentialIKControllerCfg(
                 command_type="pose",
                 use_relative_mode=True,
                 ik_method="dls",
@@ -458,7 +517,28 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
             highs=arm_highs,
             scale=scale,
         )
-
+        elif action_params.controller_type == "osc":
+            self.scene.robot.actuators["victor_left_arm"].stiffness = 0.0
+            self.scene.robot.actuators["victor_left_arm"].damping = 0.0
+            self.actions.arm_action = OperationalSpaceControllerActionCfg(
+                asset_name="robot",
+                joint_names=["victor_left_arm_joint.*"],
+                body_name="victor_left_tool0",
+                controller_cfg=OperationalSpaceControllerCfg(
+                    target_types=["pose_rel"],
+                    impedance_mode="fixed",
+                    inertial_dynamics_decoupling=True,
+                    partial_inertial_dynamics_decoupling=False,
+                    gravity_compensation=False,
+                    motion_stiffness_task=[100, 100, 100, 100, 100, 100],
+                    motion_damping_ratio_task=1.0,
+                    nullspace_control="position",
+                ),
+                nullspace_joint_pos_target="center",
+                position_scale=0.002,
+                orientation_scale=0.2,
+                stiffness_scale=100.0,
+            )
         # self.gripper_act_lows = [-0.005, -0.005]
         # self.gripper_act_highs = [0.005, 0.005]
         # self.actions.gripper_action = mdp.Robotiq3FingerActionCfg(
@@ -470,7 +550,7 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
         #     is_accumulate_action=True,
         #     keep_grasp_state=action_params.keep_grasp_state
         # )
-        nut = self.scene.nut
+        
         nut.init_state.pos = (0, 0, 0.02)
         nut.init_state.rot = (0, 1, 0, 0)
         nut_params = self.params.scene.nut
@@ -501,21 +581,27 @@ class IKRelKukaNutThreadEnvCfg(BaseNutThreadEnvCfg):
             bias_noise_cfg=GaussianNoiseCfg(mean=0.0, std=obs_params.nut_pos.bias_std, operation="abs"),
         )]
 
+        # bolt : (0.63, 0.0, 0.0)
         # for debug only
         if obs_params.use_tiled_camera:
             self.scene.tiled_camera = TiledCameraCfg(
                 prim_path="{ENV_REGEX_NS}/Camera",
                 offset=TiledCameraCfg.OffsetCfg(
-                pos=(1.3, 0.1, 0.15),
+                pos=(1., 0.1, 0.12),
                 # [ x: -2.3975473, y: 1.5188981, z: -2.3157065 ]
                 # rot=[0.4813639 , 0.5011198, 0.5182935, 0.4985375],
                 # [-90, 78, 179]
                 rot=[0.4497752 , 0.4401843, 0.5533875, 0.545621],
+                # [90, 90, 180] on rotation converter website
+                # rot=[0.5, 0.5, 0.5, 0.5],
                 convention="opengl"
+                # pos=(0.1, 0, 0.04),  # Match viewer eye position
+                # rot=(0.5, 0.5, -0.5, 0.5),  # 90 degree rotation around 
+                # convention="opengl"
             ),
             data_types=["rgb"],
             spawn=sim_utils.PinholeCameraCfg(
-                focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 20.0)
+                focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.0001, 20.0)
             ),
             width=720,
             height=720,
